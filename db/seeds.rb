3199b2e8f803dddd527e0a289d9e9ebab43f134d
jbmenashi <jbmenashi@gmail.com>
2019-01-30 10:45:05 -0500

    seed back end with api data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,86 @@
 # Bet.destroy_all
 # Ticket.destroy_all
 # User.destroy_all
-# Odd.destroy_all
+Odd.destroy_all
 Game.destroy_all
 
 def name_to_img(name)
   name.split(' ').join('_')
 end
 
+def find_correct_odds(array)
+  foundOdds = array.find {|odd| odd['OddType'] == "Game"}
+  foundOdds
+end
 
-request = RestClient.get 'https://jsonodds.com/api/odds/nhl', {'x-api-key':'c92c1650-2016-11e9-9bdc-0a704e36c1ea'}
-response = JSON.parse(request)
-response.each do |game|
-  Game.create(
+
+nhl_request = RestClient.get 'https://jsonodds.com/api/odds/nhl', {'x-api-key':'c92c1650-2016-11e9-9bdc-0a704e36c1ea'}
+nhl_response = JSON.parse(nhl_request)
+nhl_response.each do |game|
+  created_game = Game.create(
     sport: 'NHL',
     date: game['MatchTime'],
     home_team: game['HomeTeam'],
     home_logo: "https://s3.us-east-2.amazonaws.com/betski-images/#{name_to_img(game['HomeTeam'])}.png",
     away_team: game['AwayTeam'],
     away_logo: "https://s3.us-east-2.amazonaws.com/betski-images/#{name_to_img(game['AwayTeam'])}.png",
-    spread: 'test',
-    over_under: 'test',
+    spread: find_correct_odds(game["Odds"])['PointSpreadHome'],
+    over_under: find_correct_odds(game["Odds"])['TotalNumber'],
     home_score: 0,
     away_score: 0
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "moneyline",
+    home_under: find_correct_odds(game["Odds"])['MoneyLineHome'],
+    away_over: find_correct_odds(game["Odds"])['MoneyLineAway']
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "over_under",
+    home_under: find_correct_odds(game["Odds"])['UnderLine'],
+    away_over: find_correct_odds(game["Odds"])['OverLine']
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "spread",
+    home_under: find_correct_odds(game["Odds"])['PointSpreadHomeLine'],
+    away_over: find_correct_odds(game["Odds"])['PointSpreadAwayLine']
+  )
+end
+
+nba_request = RestClient.get 'https://jsonodds.com/api/odds/nba', {'x-api-key':'c92c1650-2016-11e9-9bdc-0a704e36c1ea'}
+nba_response = JSON.parse(nba_request)
+nba_response.each do |game|
+  created_game = Game.create(
+    sport: 'NBA',
+    date: game['MatchTime'],
+    home_team: game['HomeTeam'],
+    home_logo: "https://s3.us-east-2.amazonaws.com/betski-images/#{name_to_img(game['HomeTeam'])}.png",
+    away_team: game['AwayTeam'],
+    away_logo: "https://s3.us-east-2.amazonaws.com/betski-images/#{name_to_img(game['AwayTeam'])}.png",
+    spread: find_correct_odds(game["Odds"])['PointSpreadHome'],
+    over_under: find_correct_odds(game["Odds"])['TotalNumber'],
+    home_score: 0,
+    away_score: 0
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "moneyline",
+    home_under: find_correct_odds(game["Odds"])['MoneyLineHome'],
+    away_over: find_correct_odds(game["Odds"])['MoneyLineAway']
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "over_under",
+    home_under: find_correct_odds(game["Odds"])['UnderLine'],
+    away_over: find_correct_odds(game["Odds"])['OverLine']
+  )
+  Odd.create(
+    game_id: created_game.id,
+    line: "spread",
+    home_under: find_correct_odds(game["Odds"])['PointSpreadHomeLine'],
+    away_over: find_correct_odds(game["Odds"])['PointSpreadAwayLine']
   )
 end
 #
