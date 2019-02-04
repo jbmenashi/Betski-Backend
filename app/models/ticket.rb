b3c8a86bcd3ca6bc21e1003ee329bb09753e051d
jbmenashi <jbmenashi@gmail.com>
2019-02-04 16:11:24 -0500

    add validations

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ApplicationRecord
   belongs_to :user
   has_many :bets
   has_many :games, through: :bets
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :tickets
   has_many :bets, through: :tickets
+
+  validates :username, presence: true
+  validates :balance, numericality: {greater_than: -1}
 end
